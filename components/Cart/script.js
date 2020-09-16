b5b953236984976b1f022adcf886de1dcded43d0
sergiowork <sergioworkit@gmail.com>
2020-09-16 23:26:23 +0300

    feat: add method 'addToCart' in class 'Cart'

--- components/Cart/script.js
+++ components/Cart/script.js
@@ -20,4 +20,21 @@ class Cart {
     cartProduct.amount = 1;
     return cartProduct;
   }
+
+  addToCart(item) {
+    const cartProduct = this.makeCartProduct(item);
+    let hasSameItem = false;
+    const products = LSService.getCartProducts().map((el) => {
+    if (el.id === item.id) {
+      el.amount++;
+      hasSameItem = true;
+    }
+      return el;
+    });
+    if (!hasSameItem) {
+      products.push(cartProduct);
+    }
+    LSService.setCartProducts(products);
+    cartShop.render();
+  }
 }
